41e2e22653b9e075ae6694b9aa9ff9722a341466
sandeep <phalodi@gmail.com>
2015-04-07 18:01:36 +0530

    Code refactor

--- dbs/src/main/scala/com/knoldus/DBConnection/Connector.scala
+++ dbs/src/main/scala/com/knoldus/DBConnection/Connector.scala
@@ -4,7 +4,7 @@ package com.knoldus.DBConnection
 
 import reactivemongo.api.{ collections, MongoDriver }
 import scala.concurrent.ExecutionContext.Implicits.global
-import com.typesafe.config.ConfigFactory 
+import com.typesafe.config.ConfigFactory
 import reactivemongo.api.DefaultDB
 import reactivemongo.core.nodeset.Authenticate
 
@@ -13,12 +13,11 @@ trait Connector {
    * This function take the database configuration from application.conf and make connection to collection table1 of rmongo database
    * @return BSONCollection type of object
    */
-  def connector(host: String, dbName: String,userName:String,password:String): DefaultDB = {
+  def connector(host: String, dbName: String, userName: String, password: String): DefaultDB = {
     val driver = new MongoDriver
     // Gets a reference to the database
     val credentials = Seq(Authenticate(dbName, userName, password))
     val connection = driver.connection(List(host), nbChannelsPerNode = 5, authentications = credentials)
-    val db = connection(dbName)
-    db
+    connection(dbName)
   }
 }
--- dbs/src/test/scala/com/knoldus/db/ConnectionTest.scala
+++ dbs/src/test/scala/com/knoldus/db/ConnectionTest.scala
@@ -10,7 +10,7 @@ import org.scalatest._
 class ConnectorTest extends FlatSpec with Connector{
   
     "DB connection connected database" should "rmongo" in{    
-    val res=connector("localhost","rmongo","username","password").name
+    val res=connector("localhost","rmongo","rmongo","pass").name
     val expectedres= "rmongo" 
     assert(res===expectedres)
   }
