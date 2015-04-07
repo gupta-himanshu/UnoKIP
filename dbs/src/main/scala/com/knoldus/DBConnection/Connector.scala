c0d6f54fe66b919220e93cffa2ebd4cd343ffa9d
sandeep <phalodi@gmail.com>
2015-04-07 11:11:08 +0530

    Build dynamic connector for reactive mongo and code refactoring
    dynamic connector for reative mongo and code refactor(change packages)

---
+++ dbs/src/main/scala/com/knoldus/DBConnection/Connector.scala
@@ -0,0 +1,25 @@
+//DB connection
+
+package com.knoldus.DBConnection
+
+import reactivemongo.api.{ collections, MongoDriver }
+import scala.concurrent.ExecutionContext.Implicits.global
+import com.typesafe.config.ConfigFactory
+import reactivemongo.bson.BSON
+
+trait Connector {
+  /**
+   * This function take the database configuration from application.conf and make connection to collection table1 of rmongo database
+   * @return BSONCollection type of object
+   */
+  def connector(location: String, database: String, collection: String): collections.default.BSONCollection = {
+    val config = ConfigFactory.load
+    val driver = new MongoDriver
+    val connection = driver.connection(List(location))
+    // Gets a reference to the database 
+    val db = connection(database)
+    // Gets a reference to the collection
+    val coll = db(collection)
+    coll
+  }
+}
--- dbs/src/test/scala/com/knoldus/db/ConnectionTest.scala
+++ dbs/src/test/scala/com/knoldus/db/ConnectionTest.scala
@@ -1,4 +1,6 @@
-import DBConnection.Connector
+package com.knoldus.db
+
+import com.knoldus.DBConnection.Connector
 import org.scalatest._
 import reactivemongo.api.DefaultDB
 import reactivemongo.bson.BSON
@@ -6,10 +8,8 @@ import reactivemongo.bson.BSON
 class ConnectorTest extends FlatSpec with Connector{
 
     "DB connection table name" should "table1" in{    
-    val res=connector.name
+    val res=connector("localhost","rmongo","table1").name
     val expectedres="table1"
     assert(res===expectedres)
   }
-    
- 
 }
