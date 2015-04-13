7b4e248502d96ae887b1a11d4c0dbc010797721f
pushpendupurkait <pushpendupurkait@gmail.com>
2015-04-13 13:11:44 +0530

    case class Object converted to Json Data.
    case class object is converted to Json Data as well in dbs project.

--- dbs/src/main/scala/com/knoldus/converter/JsonConverter.scala
+++ dbs/src/main/scala/com/knoldus/converter/JsonConverter.scala
@@ -6,10 +6,15 @@ import org.json4s.native.JsonMethods._
 import org.json4s._
 import org.json4s.JsonDSL._
 
-trait JsonToCaseClass {
+trait JsonConverter {
   def toCaseClass(json:JsonInput):People={
     implicit val formats = org.json4s.DefaultFormats
     parse(json).extract[People]
+  }
+  
+  def toJson(people:People):String={
+    val j = ("name"-> people.name)
+    compact(render(j))
   }
 
 }
