b9538eeb61b2df747455fcaaf6368415aeb094ee
sandeep <phalodi@gmail.com>
2015-04-23 01:30:55 +0530

    Filter only english language tweets
    -filter out only english language tweets
    -fetch multiple attributes from tweets recieved in streaming

--- spark services/src/main/scala/com/knoldus/tweetstreaming/SparkStreaming.scala
+++ spark services/src/main/scala/com/knoldus/tweetstreaming/SparkStreaming.scala
@@ -4,10 +4,16 @@ import org.apache.spark.SparkContext
 import org.apache.spark.streaming.Seconds
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.twitter.TwitterUtils
-
 import com.knoldus.tweetstreaming.TwitterClient
+import twitter4j.api.HelpResources.Language
+import java.util.Date
+import twitter4j.URLEntity
+import twitter4j.Status
+import twitter4j.Place
+import twitter4j.GeoLocation
+import twitter4j.UserMentionEntity
 
-case class Tweet(tweet: String)
+case class Tweet(id: Long, date: Date, source: String, content: String,retweet: Boolean,authName:String,username:String,url: String,authId:Long,language:String)//,, longitude: Option[Double],latitude:Option[Double],,,,)
 
 /**
  * @author knoldus
@@ -15,14 +21,14 @@ case class Tweet(tweet: String)
 
 object SparkStreaming extends App {
 
-  val sparkConf: SparkConf = new SparkConf().setAppName("SparkStreaming").setMaster("local[*]")
+  val sparkConf: SparkConf = new SparkConf().setAppName("SparkStreaming").setMaster("local[2]")
   val sc: SparkContext = new SparkContext(sparkConf)
   val ssc: StreamingContext = new StreamingContext(sc, Seconds(2))
   val twitterauth = new TwitterClient().tweetCredantials()
-  val filter = Array("en")
-  val tweetDstream = TwitterUtils.createStream(ssc, Option(twitterauth.getAuthorization), filter)
-  val tweets = tweetDstream.map { x => Tweet(x.getText) }
+  val tweetDstream = TwitterUtils.createStream(ssc, Option(twitterauth.getAuthorization))
+  val tweets = tweetDstream.filter{x=>x.getUser.getLang=="en"}.map { x => Tweet(x.getId, x.getCreatedAt, x.getSource, x.getText,x.isRetweet(),x.getUser.getName,x.getUser.getScreenName,x.getUser.getURL,x.getUser.getId,x.getUser.getLang)//Some(x.getGeoLocation.getLongitude),Some(x.getGeoLocation.getLatitude))
+      }
   tweets.saveAsTextFiles("tweets/tweet")
   ssc.start()
- 
+
 }
--- spark services/src/main/scala/com/knoldus/tweetstreaming/TwitterClient.scala
+++ spark services/src/main/scala/com/knoldus/tweetstreaming/TwitterClient.scala
@@ -1,10 +1,10 @@
 package com.knoldus.tweetstreaming
 
 import com.typesafe.config.ConfigFactory
-
 import twitter4j.Twitter
 import twitter4j.TwitterFactory
 import twitter4j.auth.AccessToken
+import twitter4j.Query
 /**
  * @author knoldus
 
@@ -18,6 +18,7 @@ class TwitterClient {
   val ACCESS_TOKEN_SECRET = config.getString("twitter4j.oauth.accessTokenSecret")
   
   def tweetCredantials() = {
+    
     val twitter:Twitter = new TwitterFactory().getInstance
     twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_KEY_SECRET)
     twitter.setOAuthAccessToken(new AccessToken(ACCESS_TOKEN,ACCESS_TOKEN_SECRET))
