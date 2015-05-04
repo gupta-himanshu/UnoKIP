2eac5cf9d85ef85b95859dacfddcad3440eb2ae7
sandeep <phalodi@gmail.com>
2015-05-04 16:01:17 +0530

    change test database and add applicatio.conf for test

--- spark services/src/main/scala/com/knoldus/db/DBConnector.scala
+++ spark services/src/main/scala/com/knoldus/db/DBConnector.scala
@@ -5,10 +5,17 @@ import com.knoldus.utils.ConstantUtil.numOfChannels
 import reactivemongo.api.DefaultDB
 import reactivemongo.api.MongoDriver
 import reactivemongo.core.nodeset.Authenticate
+import com.typesafe.config.ConfigFactory
 
 trait DBConnector {
 
-  val db = connector("localhost", "rmongo", "username", "Password")
+ 
+  val config = ConfigFactory.load()
+  val host: String = config.getString("db.hostName")
+  val dbName:String=config.getString("db.dbName")
+  val username:String=config.getString("db.username")
+  val pass:String=config.getString("db.password")
+  val db = connector(host, dbName, username, pass)
 
   def connector(host: String, dbName: String, userName: String, password: String): DefaultDB = {
     val driver = new MongoDriver
--- spark services/src/test/scala/com/knoldus/db/DBServicesTest.scala
+++ spark services/src/test/scala/com/knoldus/db/DBServicesTest.scala
@@ -2,24 +2,30 @@ package com.knoldus.db
 
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.ExecutionContext.Implicits.global
 import org.scalatest.BeforeAndAfter
 import org.scalatest.FunSuite
 import com.knoldus.model.Tweet
 import com.knoldus.utils.ConstantUtil
+import reactivemongo.bson.BSONDocument
 
-class DBServicesTest extends FunSuite with DBConnector with BeforeAndAfter {
+class DBServicesTest extends FunSuite with DBConnector with BeforeAndAfter with DBServices{
+  
+before{
+    collTweet.remove(BSONDocument())
+    val res = insert(Tweet(591216111431142400L, "<a href=http://fathir.mazaa.us rel=nofollow>Aplikasi #KakakFathir</a>", "#KakakFathir Suka Menghayal ? 67", false, "Somen||48", "amat_skate48", "http://jkt48.com", 2880640850L, "en"))
+}
 
-  val dbcrud = DBServices
   test("insertion of tweets") {
-    val res = dbcrud.insert(Tweet(591216001431142400L, "<a href=http://fathir.mazaa.us rel=nofollow>Aplikasi #KakakFathir</a>", "#KakakFathir Suka Menghayal ? 67", false, "Somen||48", "amat_skate48", "http://jkt48.com", 2880640850L, "en"))
+    val res = insert(Tweet(591216001431142400L, "<a href=http://fathir.mazaa.us rel=nofollow>Aplikasi #KakakFathir</a>", "#KakakFathir Suka Menghayal ? 67", false, "Somen||48", "amat_skate48", "http://jkt48.com", 2880640850L, "en"))
     val finalRes = Await.result(res, 1 second)
     val expectedres = true
     assert(finalRes === expectedres)
   }
+  
   test("fetching tweets from mongodb") {
-    val res = dbcrud.getChunckOfTweet(1, ConstantUtil.pageSize)
+    val res = getChunckOfTweet(1, ConstantUtil.pageSize)
     val fres = Await.result(res, 5 second)
-    val exprectedres = 100
-    assert(fres.size === 100)
+    assert(fres.size === 1)
   }
 }
