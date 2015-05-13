19869a4e27921a5b0c0a5bac0416adcfdf11f8a7
pushpendupurkait <pushpendupurkait@gmail.com>
2015-05-13 16:59:30 +0530

    Time format problem solved.
    system was not taking 00:00:00 so converted into 24:00:00.

--- tweetTrends/public/javascripts/barchart.js
+++ tweetTrends/public/javascripts/barchart.js
@@ -1,70 +1,85 @@
 $(document).ready(function() {
 	$(".LoadingImage").hide();
-	WebSocketCallBar();
+	datepicker();
+	change();
+	DateChange();
 });
+//global variables
 var myVar;
+var output = document.getElementById("output");
+var now = formatDate(new Date());
+var wsUri = "ws://localhost:9000/socket?start="+ now;
 
-// WebSocket Call
-var WebSocketCallBar = (function() {
+// WebSocket Call at date change
+var DateChange = (function() {
 	$(document).ready(
 			// DatePicker OnChange event
-			$("#datetimepicker1").on("dp.change",function(e) {
-								if (myVar != undefined) {
-									clearInterval(myVar);
-									}
-								$(".LoadingImage").show();
-								$('#table-body').hide();
-								$('#container').hide();
-								/* clearInterval(myVar); */
-								var start = new Date($('#datetimepicker1').data("DateTimePicker").date());
-								var startDate = formatDate(start);
-								var wsUri = "ws://localhost:9000/socket?start="+ startDate;
-								var output = document.getElementById("output");
-
-								function WebSocketCall() {
-
-											websocket = new WebSocket(wsUri);
-											websocket.onopen = function(evt) {
-												websocket.send("ss");
-												};
-											websocket.onmessage = function(evt) {
-												var json_data = JSON.parse(evt.data);
-												barChart(json_data);
-												$('#table-body tr').remove();
-												$('#table-body').append("<tr><th>HashTags</th><th>Number</th></tr>");
-												for (i in json_data) {$('#table-body')
-															.append(
-																	"<tr>"
-																			+ "<td>"
-																			+ json_data[i][0]
-																			+ "</td>"
-																			+ "<td>"
-																			+ json_data[i][1]
-																			+ "</td>"
-																			+ "</tr>");
-												};
-												$(".LoadingImage").hide();
-												$('#table-body').show();
-												$('#container').show();
-												websocket.close();
-											};
-											websocket.onerror = function(evt) {
-												writeToScreen('<span style="color: red;">ERROR:</span> '+ evt.data);
-											};
-										}
-
-										function writeToScreen(message) {
-											var pre = document.createElement("p");
-											pre.style.wordWrap = "break-word";
-											pre.innerHTML = message;
-											output.appendChild(pre);
-										}
-										WebSocketCall();
-										myVar = setInterval(WebSocketCall,
-												11000);
-									}));
+			$("#datetimepicker1").on("dp.change",change ));
 
 });
+
+var datepicker = (function() {
+	$('#datetimepicker1').datetimepicker({
+		defaultDate:new Date(),
+		maxDate: new Date(),
+		//format: 'DD/MM/YYYY HH:mm:ss'
+	});										
+});
+
+//WebSocket call
+var change = function(){
+	if (myVar != undefined) {
+		clearInterval(myVar);
+		}
+	$(".LoadingImage").show();
+	$('#table-body').hide();
+	$('#container').hide();
+	/* clearInterval(myVar); */
+	var start = new Date($('#datetimepicker1').data("DateTimePicker").date());
+	var startDate = formatDate(start);
+	wsUri = "ws://localhost:9000/socket?start="+ startDate;
+
+	function WebSocketCall() {
+
+				websocket = new WebSocket(wsUri);
+				websocket.onopen = function(evt) {
+					websocket.send("ss");
+					};
+				websocket.onmessage = function(evt) {
+					var json_data = JSON.parse(evt.data);
+					barChart(json_data);
+					$('#table-body tr').remove();
+					$('#table-body').append("<tr><th>HashTags</th><th>Number</th></tr>");
+					for (i in json_data) {$('#table-body')
+								.append(
+										"<tr>"
+												+ "<td>"
+												+ json_data[i][0]
+												+ "</td>"
+												+ "<td>"
+												+ json_data[i][1]
+												+ "</td>"
+												+ "</tr>");
+					};
+					$(".LoadingImage").hide();
+					$('#table-body').show();
+					$('#container').show();
+					websocket.close();
+				};
+				websocket.onerror = function(evt) {
+					writeToScreen('<span style="color: red;">ERROR:</span> '+ evt.data);
+				};
+			}
+
+			function writeToScreen(message) {
+				var pre = document.createElement("p");
+				pre.style.wordWrap = "break-word";
+				pre.innerHTML = message;
+				output.appendChild(pre);
+			}
+			WebSocketCall();
+			myVar = setInterval(WebSocketCall,11000);
+		}
 
 // Date Formatting
 function formatDate(d) {
@@ -85,6 +100,11 @@ function formatDate(d) {
 
 	if (day.length == 1) {
 		day = "0" + day;
+	}
+	hh = hh + "";
+	// 00 is not taken by system so using 24.
+	if (hh == 0){
+		hh = 24;
 	}
 	return day + '/' + month + '/' + d.getFullYear() + ' ' + hh + ':' + mm
 			+ ':' + ss;
